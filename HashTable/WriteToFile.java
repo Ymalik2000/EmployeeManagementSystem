39aee0f7f99932f9f07ca7f312cccae8e469f1d9
georgeisawang <33525748+georgeisawang@users.noreply.github.com>
2017-11-17 20:54:13 -0500

    Add files via upload

---
+++ HashTable/WriteToFile.java
@@ -0,0 +1,7 @@
+public class WriteToFile {
+
+	// http://www.homeandlearn.co.uk/java/write_to_textfile.html
+	// Call this every time the hash table is modified.
+	// Make sure to replace all old contents with new contents.
+
+}
