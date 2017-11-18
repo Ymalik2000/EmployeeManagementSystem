1aff5aa33345a9c12fe9fa61fb90a41a60489c67
georgeisawang <33525748+georgeisawang@users.noreply.github.com>
2017-11-18 00:44:58 -0500

    Delete readFromFile.java

--- HashTable/readFromFile.java
+++
@@ -1,7 +0,0 @@
-public class readFromFile {
-
-	// http://www.homeandlearn.co.uk/java/read_a_textfile_in_java.html
-	// Call this at the beginning of the test program.
-	// Make sure to add all contents to the hash table.
-
-}
