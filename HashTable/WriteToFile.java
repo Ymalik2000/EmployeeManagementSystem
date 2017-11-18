416a1232e36479573501def4c7859c959448b474
georgeisawang <33525748+georgeisawang@users.noreply.github.com>
2017-11-18 00:44:49 -0500

    Delete WriteToFile.java

--- HashTable/WriteToFile.java
+++
@@ -1,7 +0,0 @@
-public class WriteToFile {
-
-	// http://www.homeandlearn.co.uk/java/write_to_textfile.html
-	// Call this every time the hash table is modified.
-	// Make sure to replace all old contents with new contents.
-
-}
